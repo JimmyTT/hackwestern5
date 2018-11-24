1ed65faac7215442d47982657a903ef6a2879a3a
p.raj <praj@uwo.ca>
2018-11-24 05:37:03 -0500

    Update trackingAlgo/trackPeople.py

--- trackingAlgo/trackPeople.py
+++ trackingAlgo/trackPeople.py
@@ -5,6 +5,10 @@ import matplotlib.pyplot as plt
 img = cv2.imread("./rockie.jpg", 1)
 #img_1 = cv2.imread("C:/Users/paava/Desktop/Projects/Hackathons/HackWestern5/rockie.jpg", 0)
 
-cv2.imshow('ImageWindow',img)
-cv2.waitKey()
+#cv2.waitKey()
 
+capture = cv2.VideoCapture(0)
+	
+frame = capture.grab()
+cv2.imshow('Window',frame)
+cv2.waitKey()
