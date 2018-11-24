301e6fa8507ce44ceee5a7256cafa3ca840695d3
p.raj <praj@uwo.ca>
2018-11-24 05:21:53 -0500

    initial files
    added the test stuff for tracking

---
+++ trackingAlgo/trackPeople.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+#img = cv2.imread("C:/Users/paava/Desktop/Projects/Hackathons/HackWestern5/rockie.jpg", 1)
+img = cv2.imread("./rockie.jpg", 1)
+#img_1 = cv2.imread("C:/Users/paava/Desktop/Projects/Hackathons/HackWestern5/rockie.jpg", 0)
+
+cv2.imshow('ImageWindow',img)
+cv2.waitKey()
+
